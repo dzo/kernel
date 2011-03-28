46f58fbe4600d946b32fcc9851dec12bfad4bd5b
thalamus <willtisdale@gmail.com>
2011-03-28 14:06:43 +0100

    ds2784_battery: Charge battery fully and drop to 98% to start charging again.

--- drivers/power/ds2784_battery.c
+++ drivers/power/ds2784_battery.c
@@ -444,12 +444,24 @@ static int battery_adjust_charge_state(struct ds2784_device_info *di)
 
 	/* shut off charger when full:
 	 * - CHGTF flag is set
+	 * - battery drawing less than 80mA
+	 * - battery at 100% capacity
+	 *
+	 * We don't move from full to not-full until
+	 * we drop below 95%, to avoid confusing the
+	 * user while we're maintaining a full charge
+	 * (slowly draining to 95 and charging back
+	 * to 100)
 	 */
-	if (di->status.status_reg & 0x80) {
+	if (di->status.percentage < 99)  {
+		di->status.battery_full = 0;
+	}
+	if ((di->status.status_reg & 0x80) &&
+	((di->status.current_avg_uA/1000) <= 40) &&
+	(di->status.percentage == 100)) {	
 		di->status.battery_full = 1;
 		charge_mode = CHARGE_BATT_DISABLE;
-	} else
-		di->status.battery_full = 0;
+	} 
 
 	if (temp >= TEMP_HOT) {
 		if (temp >= TEMP_CRITICAL)
@@ -473,7 +485,7 @@ static int battery_adjust_charge_state(struct ds2784_device_info *di)
 			charge_mode = CHARGE_BATT_DISABLE;
 	}
 
-	if (di->status.current_uA > 1024)
+	if (di->status.battery_full == 1)
 		di->last_charge_seen = di->last_poll;
 	else if (di->last_charge_mode != CHARGE_OFF &&
 		 check_timeout(di->last_poll, di->last_charge_seen, 60 * 60)) {
