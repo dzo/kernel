afc469761769813493a9d5fac935c3dcebc0340a
thalamus <willtisdale@gmail.com>
2011-04-01 12:56:40 +0100

    input: als - disable ALS level debugging messages

--- drivers/input/misc/cm3602_lightsensor_microp.c
+++ drivers/input/misc/cm3602_lightsensor_microp.c
@@ -149,8 +149,8 @@ static int get_ls_adc_level(uint8_t *data)
 					break;
 			}
 		}
-		printk(KERN_DEBUG "ALS value: 0x%X, level: %d #\n",
-				adc_value, adc_level);
+		/* printk(KERN_DEBUG "ALS value: 0x%X, level: %d #\n",
+				adc_value, adc_level); */
 		data[2] = adc_level;
 	}
 
